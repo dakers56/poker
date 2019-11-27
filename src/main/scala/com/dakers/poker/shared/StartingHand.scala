c5e755f730e5cad8fa4b18ea9a931f7c234684f3
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-11-27 06:00:13 -0500

    added upcards class

--- src/main/scala/com/dakers/poker/shared/StartingHand.scala
+++ src/main/scala/com/dakers/poker/shared/StartingHand.scala
@@ -5,3 +5,5 @@ import com.dakers.cards.Card
 abstract class StartingHand(nCards: Int)
 
 sealed case class HoleCards(cards: Card*)
+
+sealed case class UpCards(cards: Card*)
--- src/test/scala/com/dakers/poker/shared/StartingHandTest.scala
+++ src/test/scala/com/dakers/poker/shared/StartingHandTest.scala
@@ -5,24 +5,45 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class StartingHandTest extends FlatSpec with Matchers {
 
-  "A one card hand" should "have exactly one card in it" in {
+  "A one card hand of hole cards" should "have exactly one card in it" in {
     val card = Ace of Spades
     val hand = HoleCards(card)
     assert(hand.cards.length == 1)
   }
 
-  "A two card hand" should "have exactly two cards in it" in {
+  "A two card hand of hole cards" should "have exactly two cards in it" in {
     val card1 = Two of Diamonds
     val card2 = Eight of Clubs
     val hand = HoleCards(card1, card2)
     assert(hand.cards.length == 2)
   }
 
-  "A three card hand" should "have exactly three cards in it" in {
+  "A three card hand of hole cards" should "have exactly three cards in it" in {
     val card1 = Two of Diamonds
     val card2 = Eight of Clubs
     val card3 = King of Spades
     val hand = HoleCards(card1, card2, card3)
+    assert(hand.cards.length == 3)
+  }
+
+  "A one card hand of up cards" should "have exactly one card in it" in {
+    val card = Ace of Spades
+    val hand = UpCards(card)
+    assert(hand.cards.length == 1)
+  }
+
+  "A two card hand of up cards" should "have exactly two cards in it" in {
+    val card1 = Two of Diamonds
+    val card2 = Eight of Clubs
+    val hand = UpCards(card1, card2)
+    assert(hand.cards.length == 2)
+  }
+
+  "A three card hand of up cards" should "have exactly three cards in it" in {
+    val card1 = Two of Diamonds
+    val card2 = Eight of Clubs
+    val card3 = King of Spades
+    val hand = UpCards(card1, card2, card3)
     assert(hand.cards.length == 3)
   }
 }
