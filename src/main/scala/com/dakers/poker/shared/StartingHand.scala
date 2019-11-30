ea56966a632d774076fdece771d86f26c8a07b07
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-11-30 13:28:56 -0500

    added starting hand class

--- src/main/scala/com/dakers/poker/shared/StartingHand.scala
+++ src/main/scala/com/dakers/poker/shared/StartingHand.scala
@@ -2,8 +2,11 @@ package com.dakers.poker.shared
 
 import com.dakers.cards.Card
 
-abstract class StartingHand(nCards: Int)
-
-sealed case class HoleCards(cards: Card*)
-
-sealed case class UpCards(cards: Card*)
+/** Base class for starting hands. Each game should have exactly one subtype of this class representing a starting hand.
+ */
+abstract class StartingHand {
+  /**
+   * @return [[Set]] of the cards contained in this starting hand. [[Set]] is used because each card can only be dealt once.
+   */
+  def cards(): Set[Card]
+}
