27068c41dd70b854d820a10f2417eb71eb430455
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-12-03 06:28:30 -0500

    added incomplete trait

--- src/main/scala/com/dakers/poker/shared/Hand.scala
+++ src/main/scala/com/dakers/poker/shared/Hand.scala
@@ -10,11 +10,14 @@ trait Hand {
 /**
  * Used to compute possible outcomes given that this hand may have more cards. For instance, could be used to represent
  * a starting hand in NLHE that has an Ace, but the other card is not known.
+ * The idea is that if the cards and the number of "missing" cards is known, then the possibilities can be "filled in".
+ * By only specifying the number of missing cards in this trait, this trait is decoupled from consumers' implementation. If, for instance,
+ * a singleton "blank card" were used, then consumers would have an additional, potentially unnecessary, dependency.
  */
-trait IncompleteHand extends Hand {
-  val totalSize: Int
-
+trait Incomplete {
+  def missingCards(): Int
 }
+
 
 /**
  * All cards of a "complete" starting hand are known, so it should implement [[Hand.cards()]] as a val.
