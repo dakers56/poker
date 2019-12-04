1fcadd1cd177e78fb64d78faf753209bf16daa3e
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-12-04 18:48:23 -0500

    added betting actions

--- src/main/scala/com/dakers/poker/shared/Player.scala
+++ src/main/scala/com/dakers/poker/shared/Player.scala
@@ -12,17 +12,48 @@ abstract class Player {
  * Trait representing the number of chips that a player has.
  */
 trait Stack {
-  def stack: Int
+  var stack = 0
 }
+
+/**
+ * Defines the possible actions a player can take when it is their turn to bet.
+ * Does not modify the player itself because the rules of the game may not permit the action.
+ * Hence the player will "propose" one of these actions by invoking one of the methods, and the game will "validate" it.
+ */
+trait BettingActions {
+  def bet(amount: Int) = Bet(amount)
+
+  def check() = Check
+
+  def fold() = Fold
+}
+
+/**
+ * Represents a bet made by a player.
+ * @param amount size of bet
+ */
+case class Bet(amount: Int) {
+  this: Player with Stack =>
+}
+
+/**
+ * Represents a check by a player. Object because a "check" has no state.
+ */
+case object Check {
+  this: Player with Stack =>
+}
+
+/**
+ * Represents a fold by a player. Object because a "fold" has no state.
+ */
+case object Fold {
+  this: Player with Stack =>
+}
+
 
 /**
  * Trait representing a player's starting hand.
  */
 trait StartingHand {
   def startingHand: Hand
-}
-
-trait Action {
-  this : Player with Stack =>
-  def perform
 }
