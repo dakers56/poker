f2a3f851a49e2330a908e4858d2136312a62a90c
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-12-05 20:37:00 -0500

    cleanup

--- src/main/scala/com/dakers/poker/shared/Action.scala
+++ src/main/scala/com/dakers/poker/shared/Action.scala
@@ -1,5 +1,20 @@
 package com.dakers.poker.shared
 
-trait Action {
+trait Action
 
-}
+/**
+ * Represents a bet made by a player.
+ *
+ * @param amount size of bet
+ */
+class Bet(amount: Int) extends Action
+
+/**
+ * Represents a check by a player. Object because a "check" has no state.
+ */
+class Check extends Action
+
+/**
+ * Represents a fold by a player. Object because a "fold" has no state.
+ */
+class Fold extends Action
