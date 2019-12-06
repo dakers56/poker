fdfb4384e80ad4d64da89d06bef408ff93ed700e
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-12-05 20:22:18 -0500

    removed unused logic

--- src/main/scala/com/dakers/poker/shared/Player.scala
+++ src/main/scala/com/dakers/poker/shared/Player.scala
@@ -10,37 +10,6 @@ abstract class Player {
 }
 
 /**
- * Defines the possible actions a player can take when it is their turn to bet.
- * Does not modify the player itself because the rules of the game may not permit the action.
- * Hence the player will "propose" one of these actions by invoking one of the methods, and the game will "validate" it.
- */
-trait BettingActions {
-  def bet(amount: Int) = Bet(amount)
-
-  def check() = Check
-
-  def fold() = Fold
-}
-
-/**
- * Represents a bet made by a player.
- *
- * @param amount size of bet
- */
-case class Bet(amount: Int)
-
-/**
- * Represents a check by a player. Object because a "check" has no state.
- */
-case object Check
-
-/**
- * Represents a fold by a player. Object because a "fold" has no state.
- */
-case object Fold
-
-
-/**
  * Trait representing a player's starting hand.
  */
 trait StartingHand {
