d79155669b170d9179d053f86c1c3d7e1f7314dd
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-12-28 00:08:05 -0600

    streamlined game class

--- src/main/scala/com/dakers/poker/shared/Rule.scala
+++
@@ -1,3 +0,0 @@
-package com.dakers.poker.shared
-
-trait Rule
---
+++ src/main/scala/com/dakers/poker/shared/game/Game.scala
@@ -0,0 +1,7 @@
+package com.dakers.poker.shared.game
+
+import com.dakers.poker.shared.rules.Rules
+
+abstract class Game {
+  def rules: Rules
+}
