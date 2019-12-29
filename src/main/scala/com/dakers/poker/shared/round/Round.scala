28b9ed06af0db49c65fbe3a51061f4ffa8ce36aa
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-12-29 13:04:28 -0600

    removed classes that should only exist in community poker from the shared package

--- src/main/scala/com/dakers/poker/shared/round/Round.scala
+++ src/main/scala/com/dakers/poker/shared/round/Round.scala
@@ -1,27 +1,6 @@
 package com.dakers.poker.shared.round
 
-import com.dakers.cards.Card
-
 /**
  * Defines a round of betting.
  */
 abstract class Round(val name: String)
-
-/**
- * Trait representing the cards that are part of a round of betting.
- */
-trait Cards {
-  def cards: Seq[Card]
-}
-
-/** Used for betting rounds like the flop where more cards go down.
- *
- * @param name name of the round -e.g, "flop", "third street"
- */
-class RoundWithCards(name: String, val cards: Card*) extends Round(name) with Cards
-
-/** Used for betting rounds like pre-flop where no cards go down.
- *
- * @param name name of the round -e.g, "Pre-Flop"
- */
-class RoundWithoutCardsDefinition(name: String) extends Round(name)
